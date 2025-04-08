dfa028b7a8653fc906b14a289b3c0a5f32ad8758
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-08 17:49:41 +0300

    Add SectionHeader component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ import LocationSection from '../components/LocationSection';
 import ContactForm from '../components/ContactForm';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
+import SectionHeader from '../components/SectionHeader';
 
 export default function HomePage() {
   return (
@@ -33,6 +34,7 @@ export default function HomePage() {
     <ContactForm />
     <Footer />
     <Button />
+    <SectionHeader />
   </main>
       
       <footer className="py-6 bg-gray-100 dark:bg-gray-800">
